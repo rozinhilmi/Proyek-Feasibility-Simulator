568772d481d00dcd82ac0341a2d408833e8608bb
Rozin Hilmi <69004866+rozinhilmi@users.noreply.github.com>
2024-11-03 00:35:28 +0700

    Add files via upload

--- src/pages/FormCalculation/index.tsx
+++ src/pages/FormCalculation/index.tsx
@@ -9,6 +9,7 @@ import { Chart as ChartJS, LineElement, PointElement, LinearScale, CategoryScale
 import { formatNumber } from "../../utils/helper/helper";
 
 ChartJS.register(LineElement, PointElement, LinearScale, CategoryScale, Title, Tooltip, Legend);
+import { FaDownload } from "react-icons/fa6";
 
 const Index = () => {
   const navigate = useNavigate();
@@ -22,12 +23,16 @@ const Index = () => {
   });
   // const navigate = useNavigate();
   const [result, setResult] = useState<any>([]);
+  // const [breakEventPointResult, setBreakEventPointResult] = useState<any>(null);
+  // const [optimumPoint1Result, setOptimumPoint1Result] = useState<any>(null);
+  // const [optimumPoint2Result, setOptimumPoint2Result] = useState<any>(null);
 
   const inputValue = (key: string, value: string) => {
     setInputState({ ...inputState, [key]: Number(value) });
   };
-
+  const [showButtonUnduh, setShowButtonUnduh] = useState<boolean>(false);
   const calculate1 = () => {
+    setShowButtonUnduh(true);
     let hargaUnitBekas = masterCoefUnitBerkas.map((i) => inputState.priceUnit * i);
     let revenue = masterCoefPerformance.map((i) => parseFloat((inputState.revenuePerHour * masterJamKerja[0] * i).toFixed(1)));
     let mainCost = masterCoefMainCost.map((i) => inputState.periodicService * i);
@@ -69,6 +74,9 @@ const Index = () => {
     let profitCumulativePlusSellUnit = profitCumulative.map((i, index) => i + hargaUnitBekas[index]);
     let optimumPoint1 = Math.max(...profitCumulative);
     let optimumPoint2 = Math.max(...profitCumulativePlusSellUnit);
+    // setOptimumPoint1Result(optimumPoint1);
+    // setOptimumPoint2Result(optimumPoint2);
+    // setBreakEventPointResult(breakEventPoint);
 
     // console.log(JSON.stringify(profitCumulativePlusSellUnit.map((i) => customRound(i))));
 
@@ -104,8 +112,8 @@ const Index = () => {
       {
         label: "Profit Cumulative",
         data: result.map((i: any) => i.profitCumulative),
-        borderColor: "rgba(75, 192, 192, 1)",
-        backgroundColor: "rgba(75, 192, 192, 1)",
+        borderColor: "#608BC1",
+        backgroundColor: "#608BC1",
         yAxisID: "y1",
         fill: true,
         tension: 0,
@@ -134,6 +142,14 @@ const Index = () => {
     ],
   };
 
+  const handlePrint = () => {
+    setTimeout(() => {
+      setShowButtonUnduh(false);
+      window.print();
+      setShowButtonUnduh(true);
+    }, 200);
+  };
+
   return (
     <Stack
       width={"100%"}
@@ -146,7 +162,15 @@ const Index = () => {
       backgroundSize={"cover"}
       backgroundAttachment={"fixed"}
     >
+      {showButtonUnduh ? (
+        <Button onClick={handlePrint} gap={"5px"} colorScheme="green" position={"fixed"} bottom={"20px"} right={"20px"}>
+          Unduh Hasil {showButtonUnduh}
+          <FaDownload />
+        </Button>
+      ) : null}
+
       <Stack
+        id="data"
         padding={"30px"}
         backgroundColor={"#FFFFFF"}
         boxShadow={"0 2px 10px rgba(0, 0, 0, 0.1)"}
@@ -155,7 +179,7 @@ const Index = () => {
         marginY={"10vh"}
         borderRadius={"12px"}
       >
-        <Text as={"b"} fontSize={"20px"} textAlign={"center"} marginBottom={"20px"}>
+        <Text fontWeight={"bold"} fontSize={"20px"} textAlign={"center"} marginBottom={"20px"}>
           FEASIBILITY ANALYSIS HEAVY EQUIPMENT
         </Text>
         <Stack width={"100%"} maxWidth={"600px"}>
@@ -249,6 +273,145 @@ const Index = () => {
                 },
               }}
             />
+
+            <Stack gap={"5px"} marginY={"35px"} fontSize={"14px"}>
+              <Text textAlign={"center"} fontWeight={"bold"} marginBottom={"20px"}>
+                Deskripsi Grafik dan Tabel Feasibility Analysis
+              </Text>
+              <Text fontWeight={"bold"}>Grafik Profit Kumulatif dan Profit Kumulatif + Penjualan Unit</Text>
+              <HStack flexWrap={"wrap"} gap={"5px"}>
+                <Text>Grafik menunjukkan hubungan antara</Text>
+                <Text fontWeight={"bold"}>jam kerja </Text>
+                <Text>dan</Text>
+                <Text fontWeight={"bold"}>profit </Text>
+                <Text>dengan dua skenario:</Text>
+              </HStack>
+              <table>
+                <tbody>
+                  <tr>
+                    <td style={{ verticalAlign: "top" }}>1.</td>
+                    <td style={{ verticalAlign: "top" }}>
+                      <HStack flexWrap={"wrap"} gap={"5px"}>
+                        <Text fontWeight={"bold"}>Profit Kumulatif (Garis Biru):</Text>
+                        <Text> Profit tanpa penjualan tambahan.</Text>
+                      </HStack>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style={{ verticalAlign: "top" }}>2.</td>
+                    <td style={{ verticalAlign: "top" }}>
+                      <HStack flexWrap={"wrap"} gap={"5px"}>
+                        <Text fontWeight={"bold"}>Profit Kumulatif + Penjualan Unit (Garis Merah): </Text>
+                        <Text> Profit dengan tambahan penjualan unit.</Text>
+                      </HStack>
+                      <HStack flexWrap={"wrap"} gap={"5px"}>
+                        <Text fontWeight={"bold"}>• Profit naik </Text>
+                        <Text>seiring bertambahnya jam kerja hingga mencapai </Text>
+                        <Text fontWeight={"bold"}>titik tertinggi </Text>
+                        <Text>(sekitar 16.000 - 17.000 jam).</Text>
+                      </HStack>
+                      <HStack flexWrap={"wrap"} gap={"5px"}>
+                        <Text fontWeight={"bold"}>• Setelah puncak </Text>
+                        <Text>ini, profit mulai </Text>
+                        <Text fontWeight={"bold"}>menurun tajam,</Text>
+                        <Text>hingga akhirnya menjadi </Text>
+                        <Text fontWeight={"bold"}>negatif</Text>
+                        <Text>setelah jam kerja melebihi 30.000.</Text>
+                      </HStack>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <hr style={{ marginTop: "20px", marginBottom: "20px" }} />
+              <Text fontWeight={"bold"}>Tabel Profit dan Titik Penting</Text>
+              {/* <pre>{JSON.stringify(result.filter((i: any) => i.optimumPoint1 !== "")?.[0], null, 2)}</pre> */}
+
+              <table>
+                <tbody>
+                  <tr>
+                    <td style={{ verticalAlign: "top" }}>1.</td>
+                    <td style={{ verticalAlign: "top" }}>
+                      <Text fontWeight={"bold"}>Break Even Point (BEP):</Text>
+                      <HStack flexWrap={"wrap"} gap={"5px"}>
+                        <Text>• Terjadi di </Text>
+                        <Text fontWeight={"bold"}>{result.filter((i: any) => i.breakEventPoint !== "")?.[0]?.jamKerja || 0} jam</Text>
+                        <Text>dengan profit </Text>
+                        <Text fontWeight={"bold"}>{result.filter((i: any) => i.breakEventPoint !== "")?.[0]?.profitCumulativePlusSellUnit || 0}</Text>
+                        <Text>(Profit Kumulatif + Penjualan).</Text>
+                      </HStack>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style={{ verticalAlign: "top" }}>2.</td>
+                    <td style={{ verticalAlign: "top" }}>
+                      <Text fontWeight={"bold"}>Titik Optimum:</Text>
+                      <HStack flexWrap={"wrap"} gap={"5px"}>
+                        <Text>• Profit tertinggi dicapai pada </Text>
+                        <Text fontWeight={"bold"}>16.000 - 17.000 jam kerja:</Text>
+                      </HStack>
+                      <HStack flexWrap={"wrap"} marginLeft={"30px"}>
+                        <Text fontWeight={"bold"}>• Profit Kumulatif: </Text>
+                        <Text>{result.filter((i: any) => i.optimumPoint1 !== "")?.[0]?.profitCumulative || 0}</Text>
+                      </HStack>
+                      <HStack flexWrap={"wrap"} marginLeft={"30px"}>
+                        <Text fontWeight={"bold"}>• Profit Kumulatif + Penjualan: </Text>
+                        <Text>{result.filter((i: any) => i.optimumPoint2 !== "")?.[0]?.profitCumulativePlusSellUnit || 0}</Text>
+                      </HStack>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style={{ verticalAlign: "top" }}>3.</td>
+                    <td style={{ verticalAlign: "top" }}>
+                      <Text fontWeight={"bold"}>Penurunan Profit:</Text>
+                      <HStack flexWrap={"wrap"} gap={"5px"}>
+                        <Text>• Setelah</Text>
+                        <Text fontWeight={"bold"}>{result.filter((i: any) => i.optimumPoint1 !== "")?.[0]?.jamKerja || 0} jam,</Text>
+                        <Text>profit</Text>
+                        <Text fontWeight={"bold"}>turun</Text>
+                        <Text>dan akhirnya menjadi </Text>
+                        <Text fontWeight={"bold"}>rugi</Text>
+                      </HStack>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <hr style={{ marginTop: "20px", marginBottom: "20px" }} />
+              <Text fontWeight={"bold"}>Kesimpulan</Text>
+              <table>
+                <tr>
+                  <td style={{ verticalAlign: "top" }}>•</td>
+                  <td style={{ verticalAlign: "top" }}>
+                    <HStack flexWrap={"wrap"} gap={"5px"}>
+                      <Text fontWeight={"bold"}>Jam kerja ideal:</Text>
+                      <Text>Sekitar</Text>
+                      <Text fontWeight={"bold"}>16.000 - 17.000 jam </Text>
+                      <Text>untuk profit maksimal.</Text>
+                    </HStack>
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ verticalAlign: "top" }}>•</td>
+                  <td style={{ verticalAlign: "top" }}>
+                    <HStack flexWrap={"wrap"} gap={"5px"}>
+                      <Text fontWeight={"bold"}>Lewat dari itu:</Text>
+                      <Text>Profit menurun dan bisa menyebabkan </Text>
+                      <Text fontWeight={"bold"}>kerugian.</Text>
+                    </HStack>
+                  </td>
+                </tr>
+                <tr>
+                  <td style={{ verticalAlign: "top" }}>•</td>
+                  <td style={{ verticalAlign: "top" }}>
+                    <HStack flexWrap={"wrap"} gap={"5px"}>
+                      <Text fontWeight={"bold"}>Saran:</Text>
+                      <Text>Hentikan atau kurangi jam kerja setelah titik optimal untuk menghindari kerugian.</Text>
+                    </HStack>
+                  </td>
+                </tr>
+              </table>
+            </Stack>
+
             <TableContainer>
               <Table size="sm">
                 <Thead>
@@ -256,9 +419,6 @@ const Index = () => {
                     <Th border={"1px solid grey"}>Jam Kerja</Th>
                     <Th border={"1px solid grey"}>Profit Kumulatif (1)</Th>
                     <Th border={"1px solid grey"}>Profit Kumulatif + Sell Unit (2)</Th>
-                    {/* <Th>Break Event Point</Th>
-                    <Th>Optimum Point (1)</Th>
-                    <Th>Optimum Point (2)</Th> */}
                   </Tr>
                 </Thead>
                 <Tbody>
@@ -281,8 +441,6 @@ const Index = () => {
                           {i.optimumPoint2 ? <Text>(Optimum)</Text> : null}
                         </HStack>
                       </Td>
-                      {/* <Td>{i.optimumPoint1}</Td>
-                      <Td>{i.optimumPoint2}</Td> */}
                     </Tr>
                   ))}
                 </Tbody>
